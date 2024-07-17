1e7a8b875dffb52c8e65972e466dd02b9f79ee32
L2AsHdz <leonidas0197@gmail.com>
2024-07-16 20:50:12 -0600

    Api Topico

--- src/main/java/com/oracleone/topicoapi/TopicoApiApplication.java
+++ src/main/java/com/oracleone/topicoapi/TopicoApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TopicoApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TopicoApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TopicoApiApplication.class, args);
+    }
 
 }
